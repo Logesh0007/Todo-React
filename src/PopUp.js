3ad93f2cecc989bdc589c69b1052cde9ec585b47
Logeshwaran S <sremo918@gmail.com>
2022-03-31 15:17:58 +0530

    first commit

---
+++ src/PopUp.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function popUp() {
+    console.log('hello');
+    return <div style={{ width: "500px", height: '500px', backgroundColor: "red", color: 'white' }}></div>;
+}
+
+export default popUp;
