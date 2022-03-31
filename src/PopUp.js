acc20c2203e82c021c6a7a13f46cb10a23c9a842
Logeshwaran S <79743812+Logesh0007@users.noreply.github.com>
2022-03-31 15:19:09 +0530

    Delete PopUp.js

--- src/PopUp.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function popUp() {
-    console.log('hello');
-    return <div style={{ width: "500px", height: '500px', backgroundColor: "red", color: 'white' }}></div>;
-}
-
-export default popUp;
