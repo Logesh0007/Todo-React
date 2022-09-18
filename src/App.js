f40bdca456b0e6e4130f321466712e8eaad4101f
Logeshwaran S <sremo918@gmail.com>
2022-09-18 10:57:31 +0530

    New changes in todo

--- src/App.js
+++ src/App.js
@@ -1,37 +1,58 @@
-import React, { useState } from 'react';
-import './App.css'
-import Todo from './Todo';
+import React, { useState } from "react";
+import "./App.css";
+import Todo from "./Todo";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const [todo, setTodo] = useState('');
+  const [todo, setTodo] = useState("");
 
   function updTask(tasks) {
-    let newTask = tasks
-    setTasks([...newTask])
+    let newTask = tasks;
+    setTasks([...newTask]);
   }
-  const delTask = taskName => {
-    tasks.splice(tasks.indexOf(taskName), 1)
-    updTask(tasks)
-  }
+  const delTask = (taskName) => {
+    tasks.splice(tasks.indexOf(taskName), 1);
+    updTask(tasks);
+  };
 
   function addTodo(e) {
     e.preventDefault();
-    setTasks([...tasks, todo])
-    setTodo('')
+    setTasks([...tasks, todo]);
+    setTodo("");
   }
 
   return (
     <>
-      <h1 className='title' id='title'>to do app</h1>
-      <form className='ip-field' autoComplete='off'>
-        <input type='text' className='ip-todo' id='ip-todo' style={{ transition: 'all 300ms' }} value={todo} placeholder='write something' onChange={e => setTodo(e.target.value)} />
-        <button title='click to add' className='add-btn-field' onClick={addTodo} style={{ transition: "all 300ms" }}>
-          <svg fill='#38165D' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z" /></svg>
+      <h1 className="title" id="title">
+        to do app
+      </h1>
+      <form className="ip-field" autoComplete="off">
+        <input
+          type="text"
+          className="ip-todo"
+          id="ip-todo"
+          style={{ transition: "all 300ms" }}
+          value={todo}
+          placeholder="write something"
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button
+          title="click to add"
+          className="add-btn-field"
+          onClick={addTodo}
+          style={{ transition: "all 300ms" }}
+        >
+          <svg
+            fill="#38165D"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z" />
+          </svg>
         </button>
       </form>
-      <div className='list-field'>
-        <div className='list'>
+      <div className="list-field">
+        <div className="list">
           <table>
             <tbody>
               {tasks.map((task) => {
@@ -39,14 +60,14 @@ function App() {
                   <>
                     <Todo task={task} del={delTask} />
                   </>
-                )
+                );
               })}
             </tbody>
           </table>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
 
 function Todo({ task, del }) {
-    const [delClick, setDelClick] = useState(false)
+  const [delClick, setDelClick] = useState(false);
 
-    const delFunc = (e) => {
-        del(e.target.parentElement.parentElement.previousElementSibling.innerHTML)
-        setDelClick(true)
-    }
+  const delFunc = (e) => {
+    del(e.target.parentElement.parentElement.previousElementSibling.innerHTML);
+    setDelClick(true);
+  };
 
-    return (
-        <tr className='row-task' style={{ transition: 'all 300ms' }}>
-            <div className="tr-content" id="task-list">
-                {task}
-            </div>
-            <div className="opt-cont">
-
-                <svg onClick={delFunc} className="del" style={{ transition: 'all 200ms' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z" /><title>Delete</title></svg>
-            </div>
-        </tr>
-    )
+  return (
+    <tr className="row-task" style={{ transition: "all 300ms" }}>
+      <div className="tr-content" id="task-list">
+        {task}
+      </div>
+      <div className="opt-cont">
+        <svg
+          onClick={delFunc}
+          className="del"
+          style={{ transition: "all 200ms" }}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z" />
+          <title>Delete</title>
+        </svg>
+      </div>
+    </tr>
+  );
 }
 
 export default Todo;
